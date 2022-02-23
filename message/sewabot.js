8f8524ba1ea9589d8175c3f764fbeb240858170c
aowjqoidjidw <86704105+aowjqoidjidw@users.noreply.github.com>
2022-02-24 00:52:19 +0700

    Update sewabot.js

--- message/sewabot.js
+++ message/sewabot.js
@@ -4,21 +4,19 @@ return `${ucapanWaktu} kak ${pushname}
 Berikut adalah list harga untuk sewa botz
 
 *╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
-*┴*
-*│▸* 1 hari = free
-*│▸* 2 hari = Rp 2000         
-*│▸* 7 hari = Rp 5000          
-*│▸* 14 hari = Rp 10000         
-*│▸* 30 hari = Rp 20000                                           
+*┴*          
+*│▸* 14 hari = Rp.5k        
+*│▸* 30 hari = Rp.10k                   
+*│▸* Permanent = Rp.15k                    
 *┬*
 *╰────────────[*
 
 *╭─▸*「 𝗣𝗔𝗬𝗠𝗘𝗡𝗧 」
 *┴*
-*│▸* Shopee : ${settings.setPayment.Shopee}
-*│▸* Gopay : ${settings.setPayment.Gopay}
-*│▸* Dana : ${settings.setPayment.Dana}
-*│▸* Ovo : ${settings.setPayment.Ovo}
+*│▸* Shopee : ${settings.Shopee}
+*│▸* Gopay : ${settings.Gopay}
+*│▸* Dana : ${settings.Dana}
+*│▸* Ovo : ${settings.Ovo}
 *┬*
 *╰────────────[*
 
